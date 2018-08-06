37a51aaaf49cacaefd14e066b0ebd77be190333c
24nch3z <sanchezzzgg22@gmail.com>
2018-08-06 05:26:26 +0300

    Реализовал пару простых UI тестов

--- app/src/main/java/homework/smd/ru/financetracker/screens/information/presentation/InfoView.java
+++ app/src/main/java/homework/smd/ru/financetracker/screens/information/presentation/InfoView.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
-//import android.support.v4.text.HtmlCompat;
+import android.support.v4.text.HtmlCompat;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -51,7 +51,7 @@ public class InfoView extends Fragment implements InfoContract.View {
 
 	@Override
 	public void setHtmlContent(String content) {
-//		contentView.setText(HtmlCompat.fromHtml(content, HtmlCompat.FROM_HTML_MODE_COMPACT));
+		contentView.setText(HtmlCompat.fromHtml(content, HtmlCompat.FROM_HTML_MODE_COMPACT));
 	}
 
 	@Override
